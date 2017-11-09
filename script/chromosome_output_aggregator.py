d1f6cd362e0bf59ad002bfa0716f3b9529485d4d
Gustavo Rodrigues Cayres <gustavocayrespublico@gmail.com>
2017-11-09 18:33:05 -0200

    Add chromosome output aggregator

---
+++ script/chromosome_output_aggregator.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+output_path = sys.argv[1]
+chromosome_dict = dict()
+for folder_name in next(os.walk(output_path))[1]:
+    if folder_name.startswith('simulation_'):
+        simulation_folder_path = output_path + folder_name + '/'
+        for file_name in next(os.walk(simulation_folder_path))[2]:
+            file_path = simulation_folder_path + file_name
+            with open(file_path) as output_file:
+                if file_name.beginswith('cell_'):
+                    pass
+
+                else:
+                    l = []
+                    for line in output_file:
+                        l.append(int(line))
+
+                    if not chromosome_dict.get(file_name):
+                        chromosome_dict[file_name] = (l, 1)
+                    else:
+                        for i, value in enumerate(l):
+                            chromosome_dict[file_name][0][i] += value
+                            chromosome_dict[file_name][1] += 1
+
+for key, value in chromosome_dict.items():
+    with open("aggregated_{}".format(key), 'w') as aggregated_file:
+        for i in value[0]:
+            aggregated_file.write("{}\n".format(float(i/value[1])))
