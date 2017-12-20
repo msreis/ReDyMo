391344a13388488cd327248f6df14f83ab50b0a2
Gustavo Rodrigues Cayres <gustavocayrespublico@gmail.com>
2017-12-20 15:45:55 -0200

    Modify scripts

--- script/chromosome_output_aggregator.py
+++ script/chromosome_output_aggregator.py
@@ -18,13 +18,18 @@ for folder_name in next(os.walk(output_path))[1]:
             if file_name.startswith('cell'):
                 with open(file_path) as output_file:
                     line = output_file.readline()
-                    N, speed, period, time, inter_dist = line.split('\t')[0:5]
-                    data = aggregated_data.get((int(N), int(speed), int(period)), [0, 0, 0, 0, 0])
-                    data[0] += float(time)
-                    data[1] += float(inter_dist)
-                    data[2] += float(time) ** 2
-                    data[3] += float(inter_dist) ** 2
-                    data[4] += 1
+                    N, speed, period, time_1, time_2, inter_dist_1, inter_dist_2 = line.split('\t')[0:-1]
+                    data = aggregated_data.get((int(N), int(speed), int(period)), [0, 0, 0, 0, 0, 0, 0, 0, 0])
+                    data[0] += 1
+                    data[1] += float(time_1)
+                    data[2] += float(time_1) ** 2
+                    data[3] += float(time_2)
+                    data[4] += float(time_2) ** 2
+                    data[5] += float(inter_dist_1)
+                    data[6] += float(inter_dist_1) ** 2
+                    data[7] += float(inter_dist_2)
+                    data[8] += float(inter_dist_2) ** 2
+
                     aggregated_data[(int(N), int(speed), int(period))] = data
 
         for file_name in next(os.walk(simulation_folder_path))[2]:
@@ -63,11 +68,26 @@ for key, value in chromosome_dict.items():
             aggregated_file.write("{}\t{}\t\n".format(i_avg, i_sd))
 
 with open("aggregated_cell_data.txt", 'w') as aggregated_cell_file:
-    aggregated_cell_file.write("N\tspeed\ttime_avg\ttime_sd\tinter_avg\tinter_sd\tmeasurements\t\n")
+    aggregated_cell_file.write("N\tspeed\tperiod\ttime_S_avg\ttime_S_sd\tinter_S_avg\tinter_S_sd\t"
+                               "time_limit_avg\ttime_limit_sd\tinter_limit_avg\tinter_limit_sd\t"
+                               "measurements\t\n")
     for key, value in aggregated_data.items():
-        time_avg = value[0]/value[4]
-        inter_avg = value[1]/value[4]
+        time_s_avg = value[1]/value[0]
+        time_limit_avg = value[3]/value[0]
+        inter_s_avg = value[5] / value[0]
+        inter_limit_avg = value[7] / value[0]
+
+        time_s_sd = 0 if value[0] == 1 else ((value[2] / value[0] - (value[1] / value[0]) ** 2) * (value[0] / (value[0] - 1))) ** (1 / 2)
+        time_limit_sd = 0 if value[0] == 1 else ((value[4] / value[0] - (value[3] / value[0]) ** 2) * (value[0] / (value[0] - 1))) ** (1 / 2)
+        inter_s_sd = 0 if value[0] == 1 else ((value[6] / value[0] - (value[5] / value[0]) ** 2) * (value[0] / (value[0] - 1))) ** (1 / 2)
+        inter_limit_sd = 0 if value[0] == 1 else ((value[8] / value[0] - (value[7] / value[0]) ** 2) * (value[0] / (value[0] - 1))) ** (1 / 2)
         time_sd = 0 if value[4] == 1 else ((value[2] / value[4] - (value[0] / value[4]) ** 2) * (value[4] / (value[4] - 1))) ** (1 / 2)
         inter_sd = 0 if value[4] == 1 else ((value[3] / value[4] - (value[1] / value[4]) ** 2) * (value[4] / (value[4] - 1))) ** (1 / 2)
-        line = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t\n".format(key[0], key[1], time_avg, time_sd, inter_avg, inter_sd, value[4])
+        line = "{}\t{}\t{}\t" \
+               "{}\t{}\t{}\t{}\t" \
+               "{}\t{}\t{}\t{}\t"\
+               "{}\t\n".format(key[0], key[1], key[2],
+                               time_s_avg, time_s_sd, inter_s_avg, inter_s_sd,
+                               time_limit_avg, time_limit_sd, inter_limit_avg, inter_limit_sd,
+                               value[0])
         aggregated_cell_file.write(line)
