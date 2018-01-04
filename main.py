078d1e6d411fb8bebfccae6032344f22d89aa9ee
Gustavo Rodrigues Cayres <gustavocayrespublico@gmail.com>
2018-01-04 16:21:17 -0200

    Add license to files

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+
+""" This file is part of ReDyMo.
+
+    ReDyMo is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    ReDyMo is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with ReDyMo. If not, see <http://www.gnu.org/licenses/>. """
+
+""" Skeleton of the simulation process,
+    reads the input parameters from standard input,
+    starts the parallel simulations and write the results. """
+
 import os
 import sys
 from multiprocessing import Pool
@@ -9,7 +29,87 @@ from source.genome import Genome
 from source.transcription_region import TranscriptionRegion
 
 
-def output(sim_number, resources, speed, period, time, iod, percentage_log, genome):
+def parse_parameters(command_line_args):
+    number_of_repetitions = int(command_line_args[command_line_args.index('--cells') + 1])
+    organism = command_line_args[command_line_args.index('--organism') + 1]
+
+    data_manager = DataManager(database_path='data/simulation.sqlite',
+                               mfa_seq_folder_path='data/MFA-Seq_TBrucei_TREU927/')
+    chromosome_data = data_manager.chromosomes(organism=organism)
+    number_of_resources_range = (int(command_line_args[command_line_args.index('--resources') + 1]),
+                                 int(command_line_args[command_line_args.index('--resources') + 2]),
+                                 int(command_line_args[command_line_args.index('--resources') + 3]))
+    replication_fork_speed_range = (int(command_line_args[command_line_args.index('--speed') + 1]),
+                                    int(command_line_args[command_line_args.index('--speed') + 2]),
+                                    int(command_line_args[command_line_args.index('--speed') + 3]))
+
+    transcription_period_range = (int(command_line_args[command_line_args.index('--period') + 1]),
+                                  int(command_line_args[command_line_args.index('--period') + 2]),
+                                  int(command_line_args[command_line_args.index('--period') + 3]))
+
+    args_list = []
+    simulation_number = 0
+    for i in range(*number_of_resources_range):
+        for j in range(*replication_fork_speed_range):
+            for k in range(*transcription_period_range):
+                for l in range(number_of_repetitions):
+                    args_list.append({'chromosome_data': chromosome_data,
+                                      'number_of_resources': i,
+                                      'replication_speed': j,
+                                      'transcription_period': k,
+                                      'simulation_number': simulation_number})
+                    simulation_number += 1
+
+    return args_list
+
+
+def simulate(args):
+    chromosomes = []
+    for data in args['chromosome_data']:
+        transcription_regions = []
+        for region_data in data['transcription_regions']:
+            transcription_regions.append(TranscriptionRegion(start=region_data['start'],
+                                                             end=region_data['end'],
+                                                             period=args['transcription_period'],
+                                                             speed=int(2 * args['replication_speed'] / 3)))
+        chromosomes.append(Chromosome(code=data['code'],
+                                      length=data['length'],
+                                      probability_landscape=data['probability_landscape'],
+                                      replication_speed=args['replication_speed'],
+                                      transcription_regions=transcription_regions))
+
+    genome = Genome(chromosomes=chromosomes, resources=args['number_of_resources'])
+    time = 0
+    interval = 1
+    time_limit = int(1.25 * 8300)
+    percentage_log = list()
+
+    while not time > time_limit and genome.replication_percentage() < 1:
+        percentage_log.append((time, genome.replication_percentage(), genome.average_interorigin_distance()))
+
+        time += interval
+
+        genome.advance_replication_forks(interval=interval, time=time)
+
+        genome.advance_transcription_forks(interval=interval)
+
+        genome.attach_transcription_forks(interval=interval)
+
+        genome.attach_replication_forks()
+
+    write_results(
+                **{'sim_number': args['simulation_number'],
+                   'resources': args['number_of_resources'],
+                   'speed': args['replication_speed'],
+                   'period': args['transcription_period'],
+                   'time': time,
+                   'iod': genome.average_interorigin_distance(),
+                   'percentage_log': percentage_log,
+                   'genome': genome}
+                 )
+
+
+def write_results(sim_number, resources, speed, period, time, iod, percentage_log, genome):
     os.makedirs('output/', exist_ok=True)
     os.makedirs('output/simulation_{}/'.format(sim_number))
 
@@ -34,78 +134,10 @@ def output(sim_number, resources, speed, period, time, iod, percentage_log, geno
 
 
 def main(args):
-        chromosomes = []
-        for data in args['chromosome_data']:
-            transcription_regions = []
-            for region_data in data['transcription_regions']:
-                transcription_regions.append(TranscriptionRegion(start=region_data['start'],
-                                                                 end=region_data['end'],
-                                                                 period=args['transcription_period'],
-                                                                 speed=int(2*args['replication_speed']/3)))
-            chromosomes.append(Chromosome(code=data['code'],
-                                          length=data['length'],
-                                          probability_landscape=data['probability_landscape'],
-                                          replication_speed=args['replication_speed'],
-                                          transcription_regions=transcription_regions))
+    simulation_arguments = parse_parameters(command_line_args=args)
 
-        genome = Genome(chromosomes=chromosomes, resources=args['number_of_resources'])
-        time = 0
-        interval = 1
-        time_limit = int(1.25 * 8300)
-        percentage_log = list()
-
-        while not time > time_limit and genome.replication_percentage() < 1:
-            percentage_log.append((time, genome.replication_percentage(), genome.average_interorigin_distance()))
-
-            time += interval
-
-            genome.advance_replication_forks(interval=interval, time=time)
-
-            genome.advance_transcription_forks(interval=interval)
-
-            genome.attach_transcription_forks(interval=interval)
-
-            genome.attach_replication_forks()
-
-        output(sim_number=args['simulation_number'],
-               resources=args['number_of_resources'],
-               speed=args['replication_speed'],
-               period=args['transcription_period'],
-               time=time,
-               iod=genome.average_interorigin_distance(),
-               percentage_log=percentage_log,
-               genome=genome)
+    Pool(processes=40).map(simulate, simulation_arguments)
 
 
 if __name__ == '__main__':
-    number_of_repetitions = int(sys.argv[sys.argv.index('--cells') + 1])
-    organism = sys.argv[sys.argv.index('--organism') + 1]
-
-    data_manager = DataManager(database_path='data/simulation.sqlite',
-                               mfa_seq_folder_path='data/MFA-Seq_TBrucei_TREU927/')
-    chromosome_data = data_manager.chromosomes(organism=organism)
-    number_of_resources_range = (int(sys.argv[sys.argv.index('--resources') + 1]),
-                                 int(sys.argv[sys.argv.index('--resources') + 2]),
-                                 int(sys.argv[sys.argv.index('--resources') + 3]))
-    replication_fork_speed_range = (int(sys.argv[sys.argv.index('--speed') + 1]),
-                                    int(sys.argv[sys.argv.index('--speed') + 2]),
-                                    int(sys.argv[sys.argv.index('--speed') + 3]))
-
-    transcription_period_range = (int(sys.argv[sys.argv.index('--period') + 1]),
-                                  int(sys.argv[sys.argv.index('--period') + 2]),
-                                  int(sys.argv[sys.argv.index('--period') + 3]))
-
-    args_list = []
-    simulation_number = 0
-    for i in range(*number_of_resources_range):
-        for j in range(*replication_fork_speed_range):
-            for k in range(*transcription_period_range):
-                for l in range(number_of_repetitions):
-                    args_list.append({'chromosome_data': chromosome_data,
-                                      'number_of_resources': i,
-                                      'replication_speed': j,
-                                      'transcription_period': k,
-                                      'simulation_number': simulation_number})
-                    simulation_number += 1
-
-    Pool(processes=40).map(main, args_list)
+    main(sys.argv)
--- script/cell_output_aggregator.py
+++ script/cell_output_aggregator.py
@@ -1,31 +1,54 @@
-import sys
 import os
+import sys
 
 
+output_path = sys.argv[1]
 aggregated_data = dict()
-for result_file_name in next(os.walk(sys.argv[1]))[2]:
-    if result_file_name.startswith('cell_'):
-        result_path = '{}{}'.format(sys.argv[1], result_file_name)
-        with open(result_path) as result_file:
-            for line in result_file:
-                if len(line) == 1:
-                    break
+number_of_folders = 0
+for folder_name in next(os.walk(output_path))[1]:
+    if folder_name.startswith('simulation_'):
+        simulation_folder_path = output_path + folder_name + '/'
+        N = None
+        speed = None
+        period = None
+        for file_name in next(os.walk(simulation_folder_path))[2]:
+            file_path = simulation_folder_path + file_name
+            if file_name.startswith('cell'):
+                with open(file_path) as output_file:
+                    line = output_file.readline()
+                    N, speed, period, time_1, time_2, inter_dist_1, inter_dist_2 = line.split('\t')[0:-1]
+                    data = aggregated_data.get((int(N), int(speed), int(period)), [0, 0, 0, 0, 0, 0, 0, 0, 0])
+                    data[0] += 1
+                    data[1] += float(time_1)
+                    data[2] += float(time_1) ** 2
+                    data[3] += float(time_2)
+                    data[4] += float(time_2) ** 2
+                    data[5] += float(inter_dist_1)
+                    data[6] += float(inter_dist_1) ** 2
+                    data[7] += float(inter_dist_2)
+                    data[8] += float(inter_dist_2) ** 2
 
-                N, speed, time, inter_dist = line.split('\t')[0:4]
-                data = aggregated_data.get((int(N), int(speed)), [0, 0, 0, 0, 0])
-                data[0] += float(time)
-                data[1] += float(inter_dist)
-                data[2] += float(time)**2
-                data[3] += float(inter_dist)**2
-                data[4] += 1
-                aggregated_data[(int(N), int(speed))] = data
+                    aggregated_data[(int(N), int(speed), int(period))] = data
 
-with open(sys.argv[2], 'w') as output_file:
-    output_file.write("N\tspeed\ttime_avg\ttime_sd\tinter_avg\tinter_sd\tmeasurements\t\n")
+with open("aggregated_cell_data.txt", 'w') as aggregated_cell_file:
+    aggregated_cell_file.write("N\tspeed\tperiod\ttime_S_avg\ttime_S_sd\tinter_S_avg\tinter_S_sd\t"
+                               "time_limit_avg\ttime_limit_sd\tinter_limit_avg\tinter_limit_sd\t"
+                               "measurements\t\n")
     for key, value in aggregated_data.items():
-        time_avg = value[0]/value[4]
-        inter_avg = value[1]/value[4]
-        time_sd = 0 if value[4] == 1 else ((value[2] / value[4] - (value[0] / value[4]) ** 2) * (value[4] / (value[4] - 1))) ** (1 / 2)
-        inter_sd = 0 if value[4] == 1 else ((value[3] / value[4] - (value[1] / value[4]) ** 2) * (value[4] / (value[4] - 1))) ** (1 / 2)
-        line = "{}\t{}\t{}\t{}\t{}\t{}\t{}\t\n".format(key[0], key[1], time_avg, time_sd, inter_avg, inter_sd, value[4])
-        output_file.write(line)
+        time_s_avg = value[1]/value[0]
+        time_limit_avg = value[3]/value[0]
+        inter_s_avg = value[5] / value[0]
+        inter_limit_avg = value[7] / value[0]
+
+        time_s_sd = 0 if value[0] == 1 else ((value[2] / value[0] - (value[1] / value[0]) ** 2) * (value[0] / (value[0] - 1))) ** (1 / 2)
+        time_limit_sd = 0 if value[0] == 1 else ((value[4] / value[0] - (value[3] / value[0]) ** 2) * (value[0] / (value[0] - 1))) ** (1 / 2)
+        inter_s_sd = 0 if value[0] == 1 else ((value[6] / value[0] - (value[5] / value[0]) ** 2) * (value[0] / (value[0] - 1))) ** (1 / 2)
+        inter_limit_sd = 0 if value[0] == 1 else ((value[8] / value[0] - (value[7] / value[0]) ** 2) * (value[0] / (value[0] - 1))) ** (1 / 2)
+        line = "{}\t{}\t{}\t" \
+               "{}\t{}\t{}\t{}\t" \
+               "{}\t{}\t{}\t{}\t"\
+               "{}\t\n".format(key[0], key[1], key[2],
+                               time_s_avg, time_s_sd, inter_s_avg, inter_s_sd,
+                               time_limit_avg, time_limit_sd, inter_limit_avg, inter_limit_sd,
+                               value[0])
+        aggregated_cell_file.write(line)
